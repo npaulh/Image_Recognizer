3c8ad6b721b13e3a1aadbf9bc354aa9a2d12229b
Paul Hassanpour <n.paul.h@hotmail.com>
2021-12-19 20:15:55 +0100

    update packages

--- Image_Recognizer/src/views/Home.vue
+++ Image_Recognizer/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <ImageRecognizer msg="Welcome to ImageRecognizer Cloud Project"/>
+    <ImageRecognizer msg="ImageRecognizer Cloud Project"/>
   </div>
 </template>
 
